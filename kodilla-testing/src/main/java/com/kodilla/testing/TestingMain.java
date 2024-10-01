ac1e23600182c5f3f022b775b1b7570f6e226bd4
K3N-ZZO <biercmichal@gmail.com>
2024-10-01 22:41:42 +0100

    6.2 Added Calculator Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,21 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Modul 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser user = new SimpleUser("Kodilla");
+        String result = user.getUsername();
+
+        if (result.equals("Kodilla")) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Test failed");
+        }
+        Calculator calculator = new Calculator();
+        calculator.CalculatorAdd(10,50);
+        calculator.CalculatorSub(50,10);
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    public void CalculatorSub(int a, int b) {
+        c = a - b;
+        System.out.println(c);
+    }
+    public void CalculatorAdd(int a, int b) {
+        c = a + b;
+        System.out.println(c);
+    }
+}
