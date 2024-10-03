29dd93ded516be85027c007934ee55fc23a7aaee
K3N-ZZO <biercmichal@gmail.com>
2024-10-03 17:34:06 +0100

    6.3 Kodilla Testing Collection

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -3,6 +3,7 @@ import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -23,6 +24,7 @@ public class CollectionTestSuite {
     public static void afterAll() {
         System.out.println("Test suite end: ");
     }
+    @DisplayName("When Odd numbers list is empty and only should remains")
     @Test
     void testOddNumbersExterminatorEmptyList() {
         // Given
@@ -33,21 +35,24 @@ public class CollectionTestSuite {
             List<Integer> result = exterminator.exterminate(emptyList);
 
         // Then
-            Assertions.assertTrue(result.isEmpty());
+
+        Assertions.assertTrue(result.isEmpty());
     }
+    @DisplayName("When class contains odd and even numbers")
     @Test
     void testOddNumbersExterminatorNonEmptyList() {
         //Given
+
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
-        List<Integer> nonEmptyList = new ArrayList<>();
-        for (int i = 0; i <= 100; i++) {
-            nonEmptyList.add(i);
-        }
+        List<Integer> nonEmptyList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> evenResult = Arrays.asList(2,4,6,8,10);
+
         //When
+
         List<Integer> nonEmptyResult = exterminator.exterminate(nonEmptyList);
 
         //Then
-        Assertions.assertTrue(nonEmptyResult.isEmpty());
+        Assertions.assertEquals(evenResult,nonEmptyResult);
     }
 }
 
